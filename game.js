f47c8f7d7493aae4a37e5224ba19c086c31a3d83
atefeesmaelinejhad-boop <atefeesmaelinejhad@gmail.com>
2026-09-03 13:44:50 +0330

    Update game.js

--- game.js
+++ game.js
@@ -1,12 +1,1 @@
-console.log("NEXUS TEST STARTED");
-
-const faButton = document.getElementById("fa-button");
-const enButton = document.getElementById("en-button");
-
-faButton.onclick = function () {
-  alert("دکمه فارسی کار می‌کند");
-};
-
-enButton.onclick = function () {
-  alert("English button works");
-};
+alert("NEXUS GAME JS OK");
